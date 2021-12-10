99dedee69debb131b1025beda63bc63d0f6e6b5f
Jesse Chu <jwchu@cpp.edu>
2021-12-10 14:40:02 +0000

    step 53-54 + end o f chapter 3

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -20,6 +20,7 @@ enum editorKey
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -112,6 +113,7 @@ int editorReadKey()
           switch (seq[1])
           {
             case '1': return HOME_KEY;
+            case '3': return DEL_KEY;
             case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
@@ -332,6 +334,14 @@ void editorProcessKeypress()
     exit(0);
     break;
 
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+    
     case PAGE_UP:
     case PAGE_DOWN:
       {
