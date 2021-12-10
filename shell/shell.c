7ca75348a7a271a1ba2b3edba32ca0b20c2e40de
Jesse Chu <jwchu@cpp.edu>
2021-12-10 10:46:52 +0000

    fix some more minor errors

--- shell/shell.c
+++ shell/shell.c
@@ -101,9 +101,9 @@ int lsh_execute(char **args)
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++;)
+  for (i = 0; i < lsh_num_builtins(); i++)
   {
-    if (strcmp(args[0], buildin_str[i]) == 0)
+    if (strcmp(args[0], builtin_str[i]) == 0)
     {
       return (*builtin_func[i])(args);
     }
