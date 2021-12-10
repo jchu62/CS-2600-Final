634516edc9310dc90e40b67f8462fd089eefdc88
Jesse Chu <jwchu@cpp.edu>
2021-12-10 13:47:34 +0000

    step 39-40

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -170,6 +170,7 @@ void editorDrawRows(struct abuf *ab)
   {
     abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1)
     {
       abAppend(ab, "\r\n", 2);
@@ -181,12 +182,13 @@ void editorRefreshScreen()
 {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
+  abAppend(&ab, "\x1b[?25l", 6);
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6);
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
