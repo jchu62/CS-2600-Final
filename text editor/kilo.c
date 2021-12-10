e7d0151ecff45489d9faa619e31453bde40e1d23
Jesse Chu <jwchu@cpp.edu>
2021-12-10 14:37:56 +0000

    step 52

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -20,6 +20,8 @@ enum editorKey
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -109,8 +111,12 @@ int editorReadKey()
         {
           switch (seq[1])
           {
+            case '1': return HOME_KEY;
+            case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
+            case '7': return HOME_KEY;
+            case '8': return END_KEY;
           }
         }
       }
@@ -122,7 +128,17 @@ int editorReadKey()
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
+          case 'H': return HOME_KEY;
+          case 'F': return END_KEY;
         }
+      }
+    }
+    else if (seq[0] == 'O')
+    {
+      switch (seq[1])
+      {
+        case 'H': return HOME_KEY;
+        case 'F': return END_KEY;
       }
     }
     return '\x1b';
