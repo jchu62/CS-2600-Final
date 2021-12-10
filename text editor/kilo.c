f3c8028cd9bb8d842fac55d46e95df5bf6bff4fc
Jesse Chu <jwchu@cpp.edu>
2021-12-10 12:16:45 +0000

    step 17-19 + end of chapter 2

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -1,19 +1,37 @@
+/* includes */
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/* data */
+
 struct termios orig_termios;
 
-void disableRawMode()
+/* terminal */
+
+void die(const char *s)
 {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    perror(s);
+    exit(1);
+}
+void disableRawMode() 
+{
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  {
+    die("tcsetattr");
+  }
 }
 
 void enableRawMode()
 {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    {
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
     
     struct termios raw = orig_termios;
@@ -25,8 +43,13 @@ void enableRawMode()
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+    {
+        die("tcsetattr");
+    }
 }
+
+/* init*/
 
 int main() 
 {
@@ -34,7 +57,10 @@ int main()
   while (1)
   {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+    {
+        die("read");
+    }
     if (iscntrl(c)) 
     {
       printf("%d\r\n", c);
