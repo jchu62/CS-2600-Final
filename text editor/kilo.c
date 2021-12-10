613294adcd380e5fe31e9726a0bf61bd06560b58
Jesse Chu <jwchu@cpp.edu>
2021-12-10 14:16:17 +0000

    step 49-50

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -19,7 +19,9 @@ enum editorKey
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /* data */
@@ -81,22 +83,46 @@ int editorReadKey()
   {
     if (nread == -1 && errno != EAGAIN)
     {
-      die ("read");
+      die("read");
     }
   }
-if (c == '\x1b')
-{
+  if (c == '\x1b')
+  {
     char seq[3];
-    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
-    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+    {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+    {
+      return '\x1b';
+    }
     if (seq[0] == '[')
     {
-      switch (seq[1])
+      if (seq[1] >= '0' && seq[1] <= '9')
       {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+        {
+          return '\x1b';
+        }
+        if (seq[2] == '~')
+        {
+          switch (seq[1])
+          {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      }
+      else
+      {
+        switch (seq[1])
+        {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
     return '\x1b';
@@ -106,7 +132,6 @@ if (c == '\x1b')
     return c;
   }
 }
-
 
 int getCursorPosition(int *rows, int *cols) 
 {
@@ -254,16 +279,28 @@ void editorMoveCursor(int key)
   switch (key)
   {
     case ARROW_LEFT:
-    E.cx--;
+      if (E.cx != 0)
+      {
+      E.cx--;
+      }
     break;
     case ARROW_RIGHT:
-    E.cx++;
+      if (E.cx != E.screencols - 1)
+      {
+      E.cx++;
+      }
     break;
     case ARROW_UP:
-    E.cy--;
+      if (E.cy != 0)
+      {
+        E.cy--;
+      }
     break;
     case ARROW_DOWN:
-    E.cy++;
+      if (E.cy != E.screenrows - 1)
+      {
+        E.cy++;
+      }
     break;
   }
 }
