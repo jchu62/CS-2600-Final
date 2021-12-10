dde947f136f26f0aff0576f4eb47580120d02725
Jesse Chu <jwchu@cpp.edu>
2021-12-10 12:55:58 +0000

    step 25-27

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -12,8 +13,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
+struct editorConfig
+{
+  struct termios orig_termios;
+};
 
-struct termios orig_termios;
+struct editorConfig E;
 
 /* terminal */
 
@@ -26,7 +31,7 @@ void die(const char *s)
 }
 void disableRawMode() 
 {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
   {
     die("tcsetattr");
   }
@@ -34,13 +39,13 @@ void disableRawMode()
 
 void enableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     {
         die("tcgetattr");
     }
     atexit(disableRawMode);
     
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
@@ -69,11 +74,39 @@ char editorReadKey()
   return c;
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+  struct winsize ws;
+
+  if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
 /* output */
+
+void editorDrawRows()
+{
+  int y;
+  for (y = 0; y < 24; y++)
+  {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
 
 void editorRefreshScreen()
 {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
