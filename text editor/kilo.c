9f3e7ebd46aae677e35e75983fcd322497a03222
Jesse Chu <jwchu@cpp.edu>
2021-12-11 01:52:23 +0000

    step 84-88

--- text editor/kilo.c
+++ text editor/kilo.c
@@ -16,6 +16,7 @@
 
 /* defines */
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -44,6 +45,7 @@ typedef struct erow
 struct editorConfig
 {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -227,6 +229,20 @@ int getWindowSize(int *rows, int *cols)
 
 /* row operations */
 
+int editorRowCxToRx(erow *row, int cx)
+{
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++)
+  {
+    if (row->chars[j] == '\t')
+    {
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    }
+  }
+  return rx;
+}
+
 void editorUpdateRow(erow *row)
 {
   int tabs = 0;
@@ -239,14 +255,14 @@ void editorUpdateRow(erow *row)
     }
   }
   free(row->render);
-  row->render = malloc(row->size + tabs*7 + 1);
+  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
   int idx = 0;
   for (j = 0; j < row->size; j++)
   {
     if (row->chars[j] == '\t')
     {
       row->render[idx++] = ' ';
-      while (idx % 8 != 0)
+      while (idx % KILO_TAB_STOP != 0)
       {
         row->render[idx++] = ' ';
       }
@@ -331,6 +347,8 @@ void abFree(struct abuf *ab)
 
 void editorScroll()
 {
+  E.rx = E.cx;
+
   if (E.cy < E.rowoff)
   {
     E.rowoff = E.cy;
@@ -339,13 +357,13 @@ void editorScroll()
   {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff)
+  if (E.rx < E.coloff)
   {
-    E.coloff = E.cx;
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols)
+  if (E.rx >= E.coloff + E.screencols)
   {
-    E.coloff = E.cx - E.screencols + 1;
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -415,7 +433,7 @@ void editorRefreshScreen()
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-  (E.cx - E.coloff) + 1);
+  (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -518,6 +536,7 @@ void initEditor()
 {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
